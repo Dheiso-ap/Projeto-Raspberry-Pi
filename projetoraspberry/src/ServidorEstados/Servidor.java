e66b6effdd6fc6bc92f7c70a1f7275f7a7b7bd11
DHEISO ARAUJO PEREIRA <33452891+Dheiso-ap@users.noreply.github.com>
2018-11-08 12:57:54 -0200

    Update Servidor.java

--- projetoraspberry/src/ServidorEstados/Servidor.java
+++ projetoraspberry/src/ServidorEstados/Servidor.java
@@ -8,11 +8,14 @@ import java.net.Socket;
  *
  * @author dheiso
  */
+/*
+    Esta parte do código, representa uma servidor para guardar os estados dos dispositivos ativos.
+*/
 public class Servidor {
 
     public static void main(String args[]) throws IOException {
 
-        ServerSocket servidor = new ServerSocket(54321);
+        ServerSocket servidor = new ServerSocket(54321);//Abre um socket para receber conexões na porta 54321.
         ServidorNewThread threadInicial = new ServidorNewThread(null, false);
         threadInicial.registro = new ServidorRegistro();
         Thread thread = new Thread(threadInicial);
