fbab074217eb6702346f050c9c2d3dacaf394582
DHEISO ARAUJO PEREIRA <33452891+Dheiso-ap@users.noreply.github.com>
2018-12-01 11:33:06 -0200

    Update ServidorDadosMD.java

--- projetoraspberry/src/ServidorEstados/ServidorDadosMD.java
+++ projetoraspberry/src/ServidorEstados/ServidorDadosMD.java
@@ -13,6 +13,9 @@ import java.util.Calendar;
  *
  * @author Dheiso
  */
+
+/*Esta classe contem os dados para registrar um middleware,
+e um dos atributos é uma lsita com os registros das aplicações*/
 public class ServidorDadosMD {
 
     private int minuto;//este atributo é utilizado para guardar o minuto em que este registro foi atualizado
@@ -24,12 +27,12 @@ public class ServidorDadosMD {
     private Boolean validade = true;
     private ArrayList<ServidorDadosApp> dadosApp = new ArrayList<>();
 
-    public void addApp(ServidorDadosApp app) {
+    public void addApp(ServidorDadosApp app) {//adiciona o registro de uma aplicação
         this.dadosApp.add(app);
 
     }
 
-    public boolean atualizaApp(ServidorDadosApp appDados) {
+    public boolean atualizaApp(ServidorDadosApp appDados) {//atualiza o registro de uma aplicação, referente ao estado desta aplicação
         for (ServidorDadosApp registro : dadosApp) {
             if (registro.getUnicoId() == appDados.getUnicoId()) {
                 registro.setEstado(appDados.getEstado());
@@ -40,7 +43,7 @@ public class ServidorDadosMD {
         return false;
     }
 
-    public ArrayList<ServidorDadosApp> getDadosApp() {
+    public ArrayList<ServidorDadosApp> getDadosApp() {//retorna uma lista de todas as aplicações registradas no middleware
         return dadosApp;
     }
 
@@ -48,7 +51,7 @@ public class ServidorDadosMD {
         return dadosApp.isEmpty();
     }
 
-    public boolean removeApp(ServidorDadosApp app) {
+    public boolean removeApp(ServidorDadosApp app) {//remove registro de uma aplicação
         for (ServidorDadosApp registro : dadosApp) {
             if (registro.getUnicoId() == app.getUnicoId()) {
                 dadosApp.remove(registro);
@@ -59,17 +62,17 @@ public class ServidorDadosMD {
         return false;
     }
 
-    public void atualizaTempoDeAcesso() {
+    public void atualizaTempoDeAcesso() {//atualiza o momento que este registro de middleware foi atualizado
         Calendar Data = Calendar.getInstance();
         this.minuto = Data.get(Calendar.MINUTE);
         this.segundos = Data.get(Calendar.SECOND);
     }
 
-    public void removerApps() {
+    public void removerApps() {//remove todas os registro de aplicações que estão em um registro do middleware
         dadosApp.clear();
     }
 
-    public int getMinuto() {
+    public int getMinuto() {//retorna o minuto que este registro foi atualizado
         return minuto;
     }
 
@@ -77,7 +80,7 @@ public class ServidorDadosMD {
         this.minuto = minuto;
     }
 
-    public int getSegundos() {
+    public int getSegundos() {//retorna o segudno que este regitro foi atualizado
         return segundos;
     }
 
@@ -87,7 +90,7 @@ public class ServidorDadosMD {
     
     
 
-    public Boolean getValidade() {
+    public Boolean getValidade() {//retorna se a validade, que indica se o middleware está ativo ou não
         return validade;
     }
 
@@ -105,7 +108,7 @@ public class ServidorDadosMD {
         this.ipMD = ipMD;
     }
 
-    public int getUnicoId() {
+    public int getUnicoId() {//retorna a identificação unica do registro do middleware
         return unicoId;
     }
 
@@ -113,7 +116,7 @@ public class ServidorDadosMD {
         this.unicoId = unicoId;
     }
 
-    public int getTipoId() {
+    public int getTipoId() {//retorna um valro que indica qual é o tipo do middleware
         return tipoId;
     }
 
@@ -121,7 +124,7 @@ public class ServidorDadosMD {
         this.tipoId = tipoId;
     }
 
-    public int getPortaMD() {
+    public int getPortaMD() {//retorna o valor da porta em que o middlware trabalha
         return portaMD;
     }
 
@@ -129,7 +132,7 @@ public class ServidorDadosMD {
         this.portaMD = portaMD;
     }
 
-    public void deserializar(String linha) {
+    public void deserializar(String linha) {//deserializa uma string com as informações nescessarias para criar um novo objeto dessa classe
         String vetor[] = linha.split(";");
 
         this.tipoId = Integer.parseInt(vetor[0]);
